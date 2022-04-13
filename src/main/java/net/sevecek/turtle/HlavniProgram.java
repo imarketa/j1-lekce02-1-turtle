4c7f8242da5bc63a9f7ee01b01cb2ff7c860c4a0
iMarketa <marketa.kleger@gmail.com>
2022-04-13 19:56:54 +0200

    Přidaný domeček

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -2,12 +2,28 @@ package net.sevecek.turtle;
 
 import net.sevecek.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
 
         zofka = new Turtle();
+        zofka.setPenColor(Color.blue);
+        zofka.turnLeft(180);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(45);
+        zofka.move(108);
+        zofka.turnRight(90);
+        zofka.move(108);
+        zofka.turnRight(135);
+        zofka.move(150);
+
     }
 
 }
