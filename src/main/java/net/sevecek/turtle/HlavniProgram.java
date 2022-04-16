56b1824a496945cb6b22e4ebaea4c422a7c6e366
iMarketa <marketa.kleger@gmail.com>
2022-04-16 18:06:58 +0200

    Domeček

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -9,26 +9,27 @@ public class HlavniProgram {
     public void main(String[] args) {
         Turtle zofka;
 
-
-
         zofka = new Turtle();
-        zofka.setPenColor(Color.blue);
-        zofka.turnLeft(180);
-        zofka.move(150);
-        zofka.turnRight(90);
-        zofka.move(150);
-        zofka.turnRight(90);
-        zofka.move(150);
-        zofka.turnRight(45);
-        zofka.move(108);
-        zofka.turnRight(90);
-        zofka.move(108);
-        zofka.turnRight(135);
-        zofka.move(150);
-        //test
 
-        //vytvořím metodu
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(350);
 
+        zofka.setPenColor(Color.magenta);
+        zofka.penDown();
+        zofka.turnLeft (90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnLeft(120);
+        zofka.move(150);
+        zofka.turnLeft(120);
+        zofka.move(150);
+        zofka.penUp();
 
 }
 }
